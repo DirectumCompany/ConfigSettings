4506bb7d28e3538f316b52700cbf0ee16d6cc0f6
Богданова Елена (Bogdanova_ES) <Bogdanova_ES@directum.ru>
2020-10-07 12:59:07 +0500

    Добавлен класс AssemblyProductInfo для получения информации о продукте из сборки. При создании папки в AppData используется нормализованное имя продукта.

---
+++ src/ConfigSettings/Utils/AssemblyProductInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace ConfigSettings.Utils
+{
+  /// <summary>
+  /// Информация о продукте.
+  /// </summary>
+  public class AssemblyProductInfo
+  {
+    /// <summary>
+    /// Имя компании.
+    /// </summary>
+    public string CompanyName { get; }
+
+    /// <summary>
+    /// Имя продукта.
+    /// </summary>
+    public string ProductName { get; }
+
+    /// <summary>
+    /// Нормализованное (без пробелов) имя продукта.
+    /// </summary>
+    public string NormalizedProductName => this.ProductName.Replace(" ", string.Empty);
+
+    /// <summary>
+    /// Конструктор по умолчанию.
+    /// </summary>
+    /// <param name="assemblyWithInfo">Сборка, содержащая информацию о продукте.</param>
+    public AssemblyProductInfo(Assembly assemblyWithInfo)
+    {
+      var customAttributesData = assemblyWithInfo.GetCustomAttributesData();
+      this.CompanyName = (string)customAttributesData.Single(ca => ca.AttributeType == typeof(AssemblyCompanyAttribute)).ConstructorArguments.Single().Value;
+      this.ProductName = (string)customAttributesData.Single(ca => ca.AttributeType == typeof(AssemblyProductAttribute)).ConstructorArguments.Single().Value;
+    }
+  }
+}
---
+++ src/Tests/AssemblyProductInfoTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using ConfigSettings.Internal;
+using ConfigSettings.Utils;
+using NUnit.Framework;
+
+namespace ConfigSettingsTests
+{
+  [TestFixture]
+  public class AssemblyProductInfoTests
+  {
+    [Test]
+    public void TestAssemblyProductInfo()
+    {
+      var assembly = Assembly.GetExecutingAssembly();
+      var companyName = ((AssemblyCompanyAttribute)assembly.GetCustomAttributes(typeof(AssemblyCompanyAttribute)).Single()).Company;
+      var productName = ((AssemblyProductAttribute)assembly.GetCustomAttributes(typeof(AssemblyProductAttribute)).Single()).Product;
+      var assemblyProductInfo = new AssemblyProductInfo(assembly);
+      Assert.AreEqual(assemblyProductInfo.CompanyName, companyName);
+      Assert.AreEqual(assemblyProductInfo.ProductName, productName);
+      Assert.AreEqual(assemblyProductInfo.NormalizedProductName, this.NormalizeProductName(productName));
+      Assert.IsTrue(!assemblyProductInfo.NormalizedProductName.Contains(" "));
+    }
+
+    private string NormalizeProductName(string productName)
+    {
+      return productName.Replace(" ", string.Empty);
+    }
+  }
+}
